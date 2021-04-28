4819a3342207fe0eb5bee5b2dc85bc9ef4b44553
rudaks <rudaks94@gmail.com>
2021-04-28 21:21:46 +0900

    update

--- java8-in-action/src/main/java/rudaks/Test.java
+++ java8-in-action/src/main/java/rudaks/Test.java
@@ -2,8 +2,14 @@ package rudaks;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public int count;
 
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.count = 0;
+
+        System.out.println(t.count++);
+        System.out.println(++t.count);
     }
 }
 
