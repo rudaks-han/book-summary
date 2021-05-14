3731faf6fcb70ef7b442901e3e6062787de31632
rudaks <rudaks94@gmail.com>
2021-05-14 21:54:40 +0900

    update

---
+++ 아파치카프카_with자바/simple-kafka-producer/src/main/java/org/example/SimpleProducer.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProducer {
+    private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(configs);
+
+        String messageKey = "key1";
+        String messageValue = "testMessage";
+        ProducerRecord<String, String> record = new ProducerRecord(TOPIC_NAME, 2, messageKey, messageValue);
+        producer.send(record);
+        logger.info("{}", record);
+        producer.flush();
+        producer.close();
+    }
+}
