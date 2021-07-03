f05c2693018e6a13c42f28eac86a7a1309418a32
rudaks <rudaks94@gmail.com>
2021-07-03 15:53:02 +0900

    update

---
+++ 실전자바소프트웨어/src/main/java/org/example/predicate/Apple.java
@@ -0,0 +1,12 @@
+package org.example.predicate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Apple {
+    private String color;
+
+    private int weight;
+}
